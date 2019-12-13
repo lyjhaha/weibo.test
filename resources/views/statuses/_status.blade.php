a8eff18414ad3cdd1a546648bfe39b5cdc153bba
qq952051804 <952051804@qq.com>
2019-12-13 03:47:41 +0000

    关注和粉丝列表页面

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -1,17 +1,18 @@
-<li class="media mt-4 mb-4">
-  <a href="{{ route('users.show', $user->id )}}">
-    <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="mr-3 gravatar"/>
-  </a>
-  <div class="media-body">
-    <h5 class="mt-0 mb-1">{{ $user->name }} <small> / {{ $status->created_at->diffForHumans() }}</small></h5>
-    {{ $status->content }}
-  </div>
-
-  @can('destroy', $status)
-    <form action="{{ route('statuses.destroy', $status->id) }}" method="POST" onsubmit="return confirm('您确定要删除本条微博吗？');">
-      {{ csrf_field() }}
-      {{ method_field('DELETE') }}
-      <button type="submit" class="btn btn-sm btn-danger">删除</button>
-    </form>
-  @endcan
-</li>
+<a href="{{ route('users.followings', $user->id) }}">
+  <strong id="following" class="stat">
+    {{ count($user->followings) }}
+  </strong>
+  关注
+</a>
+<a href="{{ route('users.followers', $user->id) }}">
+  <strong id="followers" class="stat">
+    {{ count($user->followers) }}
+  </strong>
+  粉丝
+</a>
+<a href="{{ route('users.show', $user->id) }}">
+  <strong id="statuses" class="stat">
+    {{ $user->statuses()->count() }}
+  </strong>
+  微博
+</a>
